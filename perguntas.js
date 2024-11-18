ddfe36e70b6c9c7581c140ef0ed08de3c4defd25
thais_300 <00001105014708sp@al.educacao.sp.gov.br>
2024-11-18 17:39:49 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'One Piece',
+    'Quem é Eustass Kid?',
+    'Eustass "Capitão" Kid é um notório pirata do South Blue e o capitão dos Piratas Kid. Ele é um dos doze piratas conhecidos como a "Pior Geração"'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'One Piece',
+    'Qual é a Akuma no Mi de Eustass Kid?',
+    'Eustass Kid comeu a Jiki Jiki no Mi, uma fruta do tipo Paramecia que lhe dá o poder de manipular magnetismo'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'One Piece',
+    'Qual é a recompensa atual de Eustass Kid?',
+    'A recompensa atual de Eustass Kid é de 3.000.000.000 de berries, devido ao seu papel na derrota de Big Mom durante o Ataque à Onigashima'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'One Piece',
+    'Qual foi a primeira aparição de Eustass Kid na série?',
+    'Eustass Kid apareceu pela primeira vez no Capítulo 498 do mangá e no Episódio 392 do anime'
 )
